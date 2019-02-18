ef57b46e2fb970c5c3f05f0b8952f01e492bf82f
Nathan Thomas <nwthomas@me.com>
2019-02-18 08:21:57 -0800

    Create .get() all and .post() requests and knexfile.js

--- index.js
+++ index.js
@@ -1,17 +1,61 @@
 const express = require("express");
-const helmet = require("helmet");
-
+const knex = require("knex");
+const knexConfig = require("./knexfile.js");
 const server = express();
-
+const helmet = require("helmet");
+require("dotenv").config();
 server.use(express.json());
 server.use(helmet());
 
-// endpoints here
+const db = knex(knexConfig.development);
 
-const port = 3300;
+server.get("/", (req, res) => {
+  res.status(200).json("Dude!");
+});
+
+// Create
+server.post("/api/zoo", (req, res) => {
+  db("zoos")
+    .insert(req.body)
+    .then(ids => {
+      res.status(200).json({
+        message: "The animal was successfully added to the zoo.",
+        id: ids
+      });
+    })
+    .catch(err => {
+      res.status(500).json({
+        message: "There was an error adding animal to the zoo.",
+        error: err
+      });
+    });
+});
+
+// Read
+server.get("/api/zoo", (req, res) => {
+  db("zoos")
+    .then(animals => {
+      res.status(200).json({
+        message: "Animals retrieved from the zoo successfully.",
+        animals: animals
+      });
+    })
+    .catch(err => {
+      res.status(500).json({
+        message: "Error retrieving animals from the zoo.",
+        error: err
+      });
+    });
+});
+
+// Update
+
+// Delete
+
+const port = process.env.PORT || 4000;
 server.listen(port, function() {
   console.log(`
-  ---------------------------------------------
+  --------------------------------------------
        Listening on http://localhost:${port}
-  ---------------------------------------------`);
+  --------------------------------------------`);
 });
